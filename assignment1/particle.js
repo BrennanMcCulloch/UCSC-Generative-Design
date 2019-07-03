cf4656d0357189853ec2c1799848823c0f0ed122
BrennanMcCulloch <bmccullo@ucsc.edu>
2019-07-03 12:03:56 -0700

    OK I GOT STUFF WORKING I THINK

--- assignment1/particle.js
+++ assignment1/particle.js
@@ -1,18 +1,21 @@
 export class Particle {
 
-	constructor(force, lifetime) {
+	constructor(_force, _lifetime) {
 		this.x = random(0, width);
 		this.y = 0;
+		this.size = random(25, 150);
 		this.r = random(0, 255);
 		this.g = random(0, 255);
 		this.b = random(0, 255);
 		this.alive = false;
-		this.force = force;
-		this.lifetime = lifetime;
+		this.force = _force;
+		this.lifetime = _lifetime;
 	}
 
 	emit() {
 		this.alive = true;
-		ellipse(x, y);
+		//console.log("this whole thing makes me wanna die");
+		fill(this.r, this.g, this.b);
+		ellipse(this.x, this.y, this.size, this.size);
 	}
 }
--- assignment1/sketch.js
+++ assignment1/sketch.js
@@ -1,17 +1,41 @@
-//module.exports = require('./particle-system.js')
-//import {ParticleSystem} from '/particle-system.js';
-var timeX, timeY;
+//import ParticleSystem from '/particle-system.js';
 
+var timeX, timeY, circle;
+
+class Particle {
+
+	constructor(_force, _lifetime) {
+		this.x = random(0, width);
+		this.y = 0;
+		this.size = random(25, 150);
+		this.r = random(0, 255);
+		this.g = random(0, 255);
+		this.b = random(0, 255);
+		this.alive = false;
+		this.force = _force;
+		this.lifetime = _lifetime;
+	}
+
+	emit() {
+		this.alive = true;
+		//console.log("this whole thing makes me wanna die");
+		fill(this.r, this.g, this.b);
+		ellipse(this.x, this.y, this.size, this.size);
+	}
+}
+
+//SKETCH
 function setup() {
 	createCanvas(500, 500);
 	timeX = 0;
 	timeY = 10000;
-	var circle = new Particle(100, 100);
+	circle = new Particle(100, 100);
 }
 
 function draw() {
 	clear();
-//	circle.emit();
+	circle.emit();
+	//ellipse(100, 100, 100, 100);
 
 	var xn = noise(timeX) * width;
 	var yn = noise(timeY) * height;
@@ -20,4 +44,10 @@ function draw() {
 
 	timeX += 0.01;
 	timeY += 0.01;
+}
+
+//PUT THIS IN THE PARTICLE SYSTEM FILE
+class ParticleSystem {
+
+
 }

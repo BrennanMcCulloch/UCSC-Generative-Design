f4a8e4b7bd72ba54d0e3698d03d833f594643489
BrennanMcCulloch <bmccullo@ucsc.edu>
2019-07-03 20:52:39 -0700

    split into multiple files and arranged properly

--- assignment1/particle-system.js
+++ assignment1/particle-system.js
@@ -1,5 +1,34 @@
-import {Particle} from '/particle.js';
-export class ParticleSystem {
+//PUT THIS IN PARTICLE-SYSTEM.JS WHEN READY
+class ParticleSystem {
 
+	constructor(_particles) {
+		this.particleNum = _particles;
+		this.position = 0;
+		this.particles = [];
+		for(var x = 0; x < _particles; x++){
+			this.particles.push(new Particle(5, 100));
+		}
+	}
+
+	run(){
+		for(var x = 0; x < this.particleNum; x++){
+			this.particles[x].run();
+			if(this.particles[x].alive == false && this.particles[x].yTemp != this.particles[x].y) {
+				this.particles[x].lifetime = this.particles[x].reset;
+				this.particles[x].yTemp = this.particles[x].y;
+			}
+		}
+	}
+
+	emitOne(velocity, existence) {
+		for(var x = 0; x < this.particleNum; x++) {
+			if(this.particles[x].alive == false) {
+				this.particles[x].update(velocity, existence);
+				this.particles[x].emit();
+				break;
+			}
+		}
+	}
 
 }
+
--- assignment1/particle.js
+++ assignment1/particle.js
@@ -1,21 +1,38 @@
-export class Particle {
+//PUT IN PARTICLE.JS WHEN READY
+class Particle {
 
 	constructor(_force, _lifetime) {
+		this.size = random(5, 25);
 		this.x = random(0, width);
-		this.y = 0;
-		this.size = random(25, 150);
+		this.y = this.size / 2;
+		this.yTemp = this.y;
 		this.r = random(0, 255);
 		this.g = random(0, 255);
 		this.b = random(0, 255);
 		this.alive = false;
 		this.force = _force;
 		this.lifetime = _lifetime;
+		this.reset = this.lifetime;
 	}
 
 	emit() {
 		this.alive = true;
-		//console.log("this whole thing makes me wanna die");
-		fill(this.r, this.g, this.b);
-		ellipse(this.x, this.y, this.size, this.size);
+	}
+
+	run() {
+		if(this.alive) {
+			this.yTemp += this.force;
+			fill(this.r, this.g, this.b);
+			ellipse(this.x, this.yTemp, this.size, this.size);
+			this.lifetime -= 2;
+		}
+		if(this.lifetime <= 0) {
+			this.alive = false;
+		}
+	}
+
+	update(_force, _lifetime) {
+		this.force = _force;
+		this.lifetime += _lifetime;
 	}
 }
